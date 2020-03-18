d322627cd4fe9123cfe02a25595d1b8732895b9c
Воробьёв Дмитрий Юрьевич <DYVorobyev@alfabank.ru>
2020-03-18 15:49:50 +0300

    add files

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from data import *
+
+
+class MainView(View):
+    def get(self, request):
+        return render
+
+
+class DepartureView(View):
+    def get(self, request, deparure: str):
+        return HttpResponse('Ans')
+
+
+class TourView(View):
+    def get(self, request, id: int):
+        return HttpResponse(title)
+
