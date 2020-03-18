58795b70bcd09af2692cf503fdca67953e146225
Воробьёв Дмитрий Юрьевич <DYVorobyev@alfabank.ru>
2020-03-18 17:47:40 +0300

    add files

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Приложение 1 недели
+
+Установка (предполагается, что вы создали виртуальное окружение):
+```
+git clone ...
+pip install -r requirements.txt
+python manage.py runserver
+```
--- tours/views.py
+++ tours/views.py
@@ -1,20 +1,70 @@
 from django.shortcuts import render
 from django.views.generic import View
-
-from data import *
+from django.http import HttpResponseNotFound
+from tours.data import *
 
 
 class MainView(View):
     def get(self, request):
-        return render
+        return render(
+            request, 'tours/index.html', context={
+                "title": title,
+                "subtitle": subtitle,
+                "description": description,
+                "departures": departures,
+                "tours": tours,
+            }
+        )
 
 
 class DepartureView(View):
-    def get(self, request, deparure: str):
-        return HttpResponse('Ans')
+    def get(self, request, departure: str):
+        if departure not in departures:
+            return HttpResponseNotFound(
+                f'Вылет из {departure} не поддерживается')
+        tours_filtered = dict(
+            filter(
+                lambda x: x[1]['departure'] == departure,
+                tours.items()))
+        min_price = min(
+            tours_filtered.values(),
+            key=lambda x: x['price'])['price']
+        max_price = max(
+            tours_filtered.values(),
+            key=lambda x: x['price'])['price']
+        min_nights = min(
+            tours_filtered.values(),
+            key=lambda x: x['nights'])['nights']
+        max_nights = max(
+            tours_filtered.values(),
+            key=lambda x: x['nights'])['nights']
+        return render(
+            request, 'tours/departure.html', context={
+                "title": title,
+                "subtitle": subtitle,
+                "description": description,
+                "departures": departures,
+                "departure": departure,
+                "departure_name": departures[departure],
+                "tours": tours_filtered,
+                "min_price": min_price,
+                "max_price": max_price,
+                "min_nights": min_nights,
+                "max_nights": max_nights,
+            }
+        )
 
 
 class TourView(View):
     def get(self, request, id: int):
-        return HttpResponse(title)
-
+        if id not in tours:
+            return HttpResponseNotFound(
+                f'Тур с id {id} нам не известен')
+        return render(
+            request, 'tours/tour.html', context={
+                "title": title,
+                "departures": departures,
+                "tour": tours[id],
+                "departure_name": departures[tours[id]['departure']]
+            }
+        )
